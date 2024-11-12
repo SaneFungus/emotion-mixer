b57b0a1eee08b4f72a812b2e9ab5c7ca277045ad
SaneFungus <63272836+SaneFungus@users.noreply.github.com>
2024-11-12 13:56:02 +0100

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import EmotionMixer from '@/components/emotion-mixer-retro-react-fixed';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black p-4">
+      <div className="max-w-4xl mx-auto">
+        <EmotionMixer />
+      </div>
+    </main>
+  );
+}
